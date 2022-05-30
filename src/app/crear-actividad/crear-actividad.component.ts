ec3933b69d814d9b14f496a41e26fe8820234a44
Castro <cristobal.castro1801@alumnos.ubiobio.cl>
2022-05-29 22:41:54 -0400

    Aplicacion de los cambios de crear encuesta a crear actividad

--- src/app/crear-actividad/crear-actividad.component.ts
+++ src/app/crear-actividad/crear-actividad.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { IFormlario } from '../formulario';
+import { FormularioService } from '../services/formulario.service';
+import { ModalAddService } from '../services/modal-add.service';
 @Component({
   selector: 'app-crear-actividad',
   templateUrl: './crear-actividad.component.html',
@@ -7,15 +10,48 @@ import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 })
 export class CrearActividadComponent implements OnInit {
 
-  formFormulario:FormGroup;
+  formFormulario: FormGroup;
 
-  constructor(private formBuilder:FormBuilder) {
-    this.formFormulario=this.formBuilder.group({
-
-    })
+  constructor(
+    private formBuilder: FormBuilder,
+    private formularioService: FormularioService,
+    private modaladdService:ModalAddService
+  ) {
+    this.formFormulario = this.formBuilder.group({
+      descripcion: ['', [Validators.required]],
+      url: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      titulo: ['', [Validators.required]],
+      fechavencimiento: ['', [Validators.required]],
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  abrirModal(){
+    this.modaladdService.mostrarModal();
   }
 
+  saveData() {
+    this.abrirModal();
+    let formulario:any={
+      descripcion:this.formFormulario.value.descripcion,
+      url:this.formFormulario.value.url,
+      tipo:this.formFormulario.value.tipo,
+      titulo:this.formFormulario.value.titulo,
+      fechavencimiento:this.formFormulario.value.fechavencimiento
+    }
+    this.guardarFormulario(formulario);
+  }
+
+  guardarFormulario(formulario:IFormlario){
+    this.formularioService.saveFormulario(formulario).subscribe(() => {
+      return this.formularioService.getFormularios().subscribe((res:any[])=>{
+        this.formularioService.formularios=res
+      },
+      err=> console.log(err)
+      )
+    });
+    this.formFormulario.reset();
+  }
 }
